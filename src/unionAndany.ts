e0fef67bd0b55c70c86508510025a44647f69856
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-08-06 23:41:01 +0530

    TypeScript Practice

--- src/unionAndany.ts
+++ src/unionAndany.ts
@@ -3,3 +3,5 @@ let subs : string | number = '1M'
 let apiRequestStatus : 'pending' | 'success' | 'error' = 'pending'
 
 let airlinerequest : 'window' | 'middle' | 'right' = 'window'
+
+let finalseat;
