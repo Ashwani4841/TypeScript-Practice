11a11c4e388acb974e8a17f11d87a21c016243cf
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-08-11 23:54:09 +0530

    TypeScript Practice

--- src/type.ts
+++ src/type.ts
@@ -1,9 +1,13 @@
 // String
 let name : string = "Ashwani";
-let isAdmin : boolean = true;
-let age : number = 26;
+let city : string = "Varanasi";
+let collageName: string = "MCMT";
 
 // Number
 let age: number = 25;
 let salary: number = 30000;
 let price: number = 599.99;
+
+// boolean
+let isLoggedIn: boolean = true;
+let isAdmin: boolean = false;
